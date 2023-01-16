5158473ca14565ad2b16ac7cff5359209fb58fe1
Victor <victorgaspalmeida@hotmail.com>
2023-01-16 16:27:25 -0300

    sidebar pronto

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from "./Sidebar.module.css"
+import { Pencil, PencilLine } from "phosphor-react";
 
 export function Sidebar(){
     return (
@@ -6,12 +7,13 @@ export function Sidebar(){
             <img className={styles.cover} src="https://images.unsplash.com/photo-1546198633-17b6085e28dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"/>
 
             <div className={styles.profile}>
+                <img src="https://avatars.githubusercontent.com/u/87485590?v=4" />
                 <strong> Vitin </strong>
                 <span>Full-Stack Developer</span>
             </div>
 
-            <footer>
-                <a href="#">Edite seu Perfil</a>
+            <footer>        
+                <a href="#"><PencilLine size={20}/> Edite seu Perfil</a>
             </footer>
         </aside>
     )
